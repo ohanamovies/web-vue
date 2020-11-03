9f4fb6a325cb9e8068411f926f1034f6ab91a272
asopenag <alex.sopena.91@gmail.com>
2020-11-03 01:08:23 +0100

    First approach to shield in content page

---
+++ src/assets/raw_tags.js
@@ -0,0 +1,192 @@
+var content = [
+  {
+    value: 'Sex/Nudity',
+    title: 'Sex/Nudity',
+    description: '',
+    color: 'green',
+    severity_title: 'How graphic or erotic is it?',
+    types_title: 'Does it contain any of the following?',
+    severity: [
+      {
+        value: 'Non erotic',
+        title: 'Non erotic',
+        description:
+          "Nudity with no erotic purpose, eg: Michelangelo's David, human corpse..."
+      },
+      {
+        value: 'Slightly erotic',
+        title: 'Slightly erotic',
+        description:
+          'Slightly erotic or graphic scene, eg: brief kiss, revealing outfit, mild sexual reference...'
+      },
+      {
+        value: 'Moderately erotic',
+        title: 'Moderately erotic',
+        description:
+          'Moderately erotic or graphic scene, eg: passionate kissing, provocative dancing, graphic sexual remarks...'
+      },
+      {
+        value: 'Very erotic',
+        title: 'Very erotic',
+        description:
+          'Very erotic or graphic scene, eg: sex, foreplay, moaning...'
+      }
+    ],
+    types: [
+      {
+        value: 'No consent',
+        title: 'No consent',
+        description:
+          'Lack or inability to consent, eg: rape, child molestation, harassment...'
+      },
+      {
+        value: 'Unloving/objectifying',
+        title: 'Unloving/objectifying',
+        description:
+          'Limited intention to bond, care or look after the other, eg: one night stand, prostitution, striptease...'
+      },
+      {
+        value: 'Non-procreative',
+        title: 'Non-procreative',
+        description:
+          'Sexual intercourse outside a procreative context, eg: oral sex, anal sex, masturbation, explicit use of contraception...'
+      },
+      {
+        value: 'Infidelity',
+        title: 'Infidelity',
+        description: 'Unfaithfulness to a partner, eg: adultery...'
+      }
+    ]
+  },
+  {
+    value: 'Violence',
+    title: 'Violence/Gore',
+    description: '',
+    color: 'red',
+    severity_title: 'How graphic/gore is it?',
+    types_title: 'Does it contain any of the following?',
+    severity: [
+      {
+        value: 'Non gore',
+        title: 'Non gore',
+        description: 'Violence is implied but nothing is shown'
+      },
+      {
+        value: 'Slightly gore',
+        title: 'Slightly gore',
+        description:
+          'Slightly gore or graphic scene, eg: mild verbal violence, punching'
+      },
+      {
+        value: 'Moderately gore',
+        title: 'Moderately gore',
+        description:
+          'Moderately gore or graphic scene, eg: extensive bleeding, broken bones...'
+      },
+      {
+        value: 'Very gore',
+        title: 'Very gore',
+        description:
+          'Very gore or graphic scene, eg: blood splattered, open wounds, guts...'
+      }
+    ],
+    types: [
+      {
+        value: 'Discrimination',
+        title: 'Discrimination',
+        description:
+          'Verbal of physical violence due to race, sex, religion, disability...'
+      },
+      {
+        value: 'Cruelty',
+        title: 'Cruelty',
+        description:
+          'One of the parties is significantly weaker or have no chance of winning'
+      },
+      {
+        value: 'Power imbalance',
+        title: 'Power imbalance',
+        description:
+          'One of the parties is significantly weaker or have no chance of winning'
+      },
+      {
+        value: 'Self harm',
+        title: 'Self harm',
+        description: 'Self inflicted pain or damage, eg: suicide...'
+      }
+    ]
+  },
+  {
+    value: 'Other',
+    title: 'Other',
+    description: '',
+    color: 'blue',
+    severity_title: '',
+    types_title: 'Which of the following does it contain?',
+    severity: [],
+    types: [
+      {
+        value: 'Mild profanity',
+        title: 'Mild profanity',
+        description: 'Mild language, eg: h*ll, d*mn...'
+      },
+      {
+        value: 'Strong profanity',
+        title: 'Strong profanity',
+        description: 'Swear words'
+      },
+      {
+        value: 'Blasphemy',
+        title: 'Blasphemy',
+        description: 'Speaking sacrilegiously about God or sacred things'
+      },
+      {
+        value: 'Legal drugs',
+        title: 'Legal drugs',
+        description: 'Consumption of legal drugs, eg: alcohol, tobacco...'
+      },
+      {
+        value: 'Illegal drugs',
+        title: 'Illegal drugs',
+        description:
+          'Consumption or dealing of illegal drugs, eg: weed, cocaine...'
+      }
+    ]
+  }
+]
+
+var actions = {
+  types_title: 'Anything else?',
+  types: [
+    {
+      value: 'Sound only',
+      title: 'Sound only',
+      description: 'Tags apply only to sound'
+    },
+    {
+      value: 'Video only',
+      title: 'Video only',
+      description: 'Tags apply only to video.'
+    },
+    {
+      value: 'Local',
+      title: 'Local',
+      description: 'Store only on your computer. Do not share it with others.'
+    },
+    {
+      value: 'Mild plot',
+      title: 'Mild plot',
+      description: 'This scene is slightly important for the plot.'
+    },
+    {
+      value: 'Strong plot',
+      title: 'Strong plot',
+      description: 'This scene is very important for the plot.'
+    }
+  ]
+}
+
+module.exports = {
+  content,
+  actions
+}
--- src/views/Content.vue
+++ src/views/Content.vue
@@ -4,19 +4,48 @@
     <section class="wrapper" id="main">
       <div class="inner">
         <h4>Discover content</h4>
-        <p style="margin-bottom:0px">
-          <span
-            style="text-decoration:underline; cursor:pointer"
-            @click="type = 'movie'"
-            >Movies</span
+        <p>Please, first check the kind of content that you want to avoid</p>
+
+        <div id="your-filters" class="row">
+          <div
+            v-for="(tag, index) in tags"
+            :key="index"
+            class="3u 6u(medium) 6u$(small)"
           >
-          |
-          <span
-            style="text-decoration:underline;cursor:pointer"
-            @click="type = 'show'"
-            >Shows</span
-          >
-        </p>
+            <div v-if="tag.severity.length > 0">
+              <b>{{ tag.title }}</b>
+              <div v-for="(sev, index) in tag.severity" :key="index">
+                <input
+                  type="checkbox"
+                  :id="sev.value"
+                  :value="sev.value"
+                  :name="sev.value"
+                  v-model="caringTags"
+                />
+
+                <label :for="sev.value">{{ sev.title }}</label>
+              </div>
+            </div>
+          </div>
+
+          <!-- <p>{{ caringTags }}</p> -->
+        </div>
+
+        <div id="typeContent">
+          <p style="margin-bottom:0px">
+            <span
+              style="text-decoration:underline; cursor:pointer"
+              @click="type = 'movie'"
+              >Movies</span
+            >
+            |
+            <span
+              style="text-decoration:underline;cursor:pointer"
+              @click="type = 'show'"
+              >Shows</span
+            >
+          </p>
+        </div>
 
         <div class="posters_wrapper">
           <div class="poster_card" v-for="(item, index) in items" :key="index">
@@ -33,7 +62,14 @@
               <!-- poster a secas -->
               <img v-else :src="item.data.metadata.poster" alt="" />
             </div>
-            <b>{{ item.data.metadata.title }}</b>
+            <div class="content">
+              <div class="shield">
+                <img :src="getShieldImage(item)" alt="" width="50px" />
+              </div>
+              <b>{{ item.data.metadata.title }}</b>
+              <br />
+              {{ getCaringScenes(item) }}
+            </div>
           </div>
         </div>
       </div>
@@ -42,12 +78,20 @@
 </template>
 
 <script>
+//rawTags = require('../js/raw_tags')
 export default {
   data() {
     return {
       data: [],
       loading: false,
-      type: 'movie'
+      type: 'movie',
+      tags: require('@/assets/raw_tags').content,
+      caringTags: []
+    }
+  },
+  watch: {
+    caringTags(newValue) {
+      localStorage.caringTags = JSON.stringify(newValue)
     }
   },
   computed: {
@@ -62,6 +106,67 @@ export default {
     }
   },
   methods: {
+    getCaringScenes(item) {
+      //return 'kaixo there' + item.data.metadata.title
+      var caringScenes = {}
+      if (item.data) {
+        if (item.data.scenes) {
+          item.data.scenes.forEach(scene => {
+            this.caringTags.forEach(ct => {
+              if (scene.tags.includes(ct)) {
+                if (!caringScenes[ct]) {
+                  caringScenes[ct] = 0
+                }
+                caringScenes[ct] = caringScenes[ct] + 1
+              }
+            })
+          })
+        }
+      }
+
+      var taggedAux = {}
+      this.caringTags.forEach(ct => {
+        if (item.data.tagged[ct]) {
+          taggedAux[ct] = item.data.tagged[ct].status
+        }
+      })
+
+      return taggedAux
+    },
+    getShieldImage(item) {
+      var shield = this.getShield(item)
+      return '/fc/' + shield + '.png'
+    },
+    getShield(item) {
+      var finalStatus = 3 //1:missing, 2: unknown, 3:protected
+
+      if (!item.data.tagged) {
+        finalStatus = 2
+      } else {
+        this.caringTags.forEach(ct => {
+          var auxx
+          if (!item.data.tagged[ct]) {
+            auxx = 2
+          } else {
+            var s = item.data.tagged[ct].status
+            auxx = s == 'done' ? 3 : s == 'missing' ? 1 : 2
+          }
+          if (auxx < finalStatus) {
+            finalStatus = auxx
+          }
+        })
+      }
+
+      //console.log(finalStatus, item)
+      //getShield(item)
+      var shield =
+        finalStatus == 3
+          ? 'protected'
+          : finalStatus == 1
+          ? 'missing'
+          : 'unknown'
+      return shield
+    },
     listType(type) {
       var auxx = []
       this.data.forEach(e => {
@@ -87,6 +192,10 @@ export default {
   },
   mounted() {
     this.getData()
+    var xx = localStorage.caringTags
+    if (xx && xx != 'undefined') {
+      this.caringTags = JSON.parse(xx)
+    }
   }
 }
 </script>
@@ -126,5 +235,25 @@ div.posters_wrapper div.poster_card {
 div.posters_wrapper div.poster_card div.image img {
   width: 100%;
   /*height: 100%;*/
+}
+
+div.posters_wrapper div.poster_card div.content {
+  width: 100%;
+  padding: 26px 10px 12px 10px;
+  position: relative;
+  white-space: normal;
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
+}
+
+div.posters_wrapper div.poster_card div.content div.shield {
+  position: absolute;
+  top: -25px;
+  right: 0px;
+
+  width: 38px;
+  height: 38px;
+  box-sizing: border-box;
 }
 </style>
