300de73f5c041255363f2fc3864af124a1f591b9
miguel <arrietaeguren@gmail.com>
2021-06-21 09:20:48 +0200

    some int explanations

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,3 +1,21 @@
+
+/* How to translate a text
+
+On the code. Replace the text to be translated with:
+  a) {{ $t('whatIsOhana') }} if it does NOT contain html tags (<p>, <b>, <a> ...)
+  b) <span v-html="$t('whatIsOhana')"></span> otherwise
+
+On this file. Add the text under each language, eg:
+  en: {
+    whatIsOhana: 'What is Ohana?',
+    ....
+  },
+  es: {
+    whatIsOhana: '¿Qué es Ohana?',
+    ...
+  },
+*/
+
 const messages = {
   en: {
     whatIsOhana: 'What is Ohana?',
