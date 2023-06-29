da50ce5a0428198031e250a9461599675c4b998c
miguel <arrietaeguren@gmail.com>
2023-06-29 18:56:23 +0200

    Add todo

---
+++ TODO.md
@@ -0,0 +1,22 @@
+# Ohana TV - TODO
+
+## Blocker [P0]
+
+## Priority 1 [P1]
+
+- Skip Tags: improve desgin && translate
+- fix #cancel and #donated on home (redirected from donation screen)
+- Finish values
+- Common files (git submodule)
+- Load more on scroll home
+
+## Nice to have [P2]
+
+- Auto insert imdbTable -> movieTable (SOP)
+- Filter adult content on API (API)
+
+## Future [P3]
+
+- Enable search by genre... eg: "genre:action sort:imdb-votes "
+- Donate popup on extension
+- Create watchlist and watched lists
--- src/assets/ohana/ohana_modules/movies.js
+++ src/assets/ohana/ohana_modules/movies.js
@@ -62,7 +62,7 @@ const movies = {
         // This test all languages, so there should be no need to translate the filters
         //if (movie.imdbRating && movie.imdbRating < 3) return true
         //if (/hot/.test(title)) return true
-        return /porn|sex|nude|naked|nudity|erotic|gay|lust|whore|breast|tetas|slut|vagina|penis|lingerie|tits|fuck/.test(
+        return /\bfetish|\bporn|\bsex|\bnude|\bnaked|\bnudity|\berotic|\bgay|\blust|\bwhore|\bbreast|\btetas|\bslut|\bvagina|\bpenis|\blingerie|\btits|\bfuck/.test(
           text
         )
       }
